45259d976b347a63d4d698a73c99fb7b39b5b9bc
fraws13 <fallocca12@gmail.com>
2026-03-30 17:21:11 +0200

    Refactor peppe class to include a printPeppe method and modify main loop to print "peppe" continuously.

--- peppe.java
+++ peppe.java
@@ -1,7 +1,10 @@
 public class peppe{
     public static void main(String[] args) {
-    while (true){
+        while (true){
+            System.out.println("peppe");
+        }
+    }
+    public static void printPeppe() {
         System.out.println("peppe");
     }
-}
 }
